9c6e7b4defbcb50d677281f7b7d96b8e1fb4ab4f
Jatin Gupta <jatin.gupta@kgpian.iitkgp.ac.in>
2022-11-28 22:29:02 +0530

    added notes service

--- backend/app/routers/notes.py
+++ backend/app/routers/notes.py
@@ -1,5 +1,8 @@
+import base64
 from fastapi import APIRouter
-from ..services.notes import testing
+
+from flask import send_file
+from ..services.notes import note_make, testing
 
 router = APIRouter(
     prefix="/notes",
@@ -9,3 +12,19 @@ router = APIRouter(
 @router.get("/")
 def notes_check():
     return testing()
+
+@router.get("/image")
+def img_to_bb():
+    img_src = note_make(just_img=True)
+    response = send_file(img_src, "image/jpg", attachment_filename='notes_output_img.jpg', as_attachment=True)
+    response.headers["x-filename"] = 'notes_output_img.jpg'
+    response.headers["Access-Control-Expose-Headers"] = 'x-filename'
+    return response
+
+@router.get("/audio")
+def note_to_audio(url:str):
+    audio_path = note_make(url)
+    with open(audio_path, 'rb') as audio_file:
+        encoded_file = base64.b64encode(audio_file.read())
+    image_path = note_make(just_img=True)
+    return {'audio':encoded_file.decode('utf-8', errors='replace'), 'image':image_path}
