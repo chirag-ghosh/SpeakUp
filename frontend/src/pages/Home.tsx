e40d2d06bff027ac42b74e02b6849afd293e1ded
chirag-ghosh <cghosh828049@gmail.com>
2022-11-28 17:46:53 +0530

    Add homepage;

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div className="home">This website is a work in progress.</div>;
+  return (
+    <div className="home">
+      <h1 className="title">SpeakUp</h1>
+      <div className="subtitle">A step towards making communication easier.</div>
+      <div className="subtitle">Please click the icons below to explore.</div>
+    </div>
+  );
 }
 
 export default Home;
